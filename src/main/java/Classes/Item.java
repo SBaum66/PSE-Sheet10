eff1f5ae3e45d746d1bfe56cb3c9851b20f27f1d
Stefan <baumst@ks1s.onedrive.live.com>
2023-01-18 21:34:15 +0100

    Versandhaus

--- src/main/java/Classes/Item.java
+++ src/main/java/Classes/Item.java
@@ -1,16 +1,20 @@
 package Classes;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public class Item {
     private String name;
     private String id;
     private Integer barcode;
-
+    //private ArrayList<Optional<Item>> items;
     public Item(String name) {
         this.name = name;
         this.id = UUID.randomUUID().toString();
-    }
+        this.barcode = (int) (Math.random() * 1000000000);
+        }
     public String getName() {
         return name;
     }
@@ -18,7 +22,7 @@ public class Item {
     public String getId() {
         return id;
     }
-    public Integer getBarcode() {
+    public Number getBarcode() {
         return barcode;
     }
 
--- src/main/java/baum/aydin/elkhouly/Main.java
+++ src/main/java/baum/aydin/elkhouly/Main.java
@@ -3,12 +3,19 @@ import Classes.Item;
 import Classes.ItemIdentification;
 import Classes.Warehouse;
 
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.UUID;
+
 public class Main {
     public static void main(String[] args) {
         Warehouse amazon = new Warehouse(9);
         Item stift = new Item("stift");
         amazon.addItem(stift);
+        ArrayList<Optional<Item>> items = new ArrayList<Optional<Item>>();
+
         String i = amazon.getinventory().toString();
-        System.out.println(i);
+        System.out.println(items);
+        System.out.println(UUID.randomUUID());
     }
 }
