f55a6f872ed6e5881a77c05faf0d65e0f78f595c
Stefan <you@example.cum>
2023-01-18 12:01:00 +0100

    Maven Project with Warehouse Class and Main methode

---
+++ src/main/java/Classes/Warehouse.java
@@ -0,0 +1,8 @@
+package Classes;
+
+import java.awt.*;
+
+public class Warehouse {
+    private List inventory = new List();
+    private Integer capacity = new Integer(0);
+}
---
+++ src/main/java/baum/aydin/elkhouly/Main.java
@@ -0,0 +1,8 @@
+package baum.aydin.elkhouly;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("lol");
+    }
+}
