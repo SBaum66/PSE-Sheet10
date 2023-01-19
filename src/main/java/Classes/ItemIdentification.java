fab2539867fd193b832e86bd7415175a590728ad
Stefan <you@example.cum>
2023-01-19 14:51:29 +0100

    aufgabe d

--- src/main/java/Classes/ItemIdentification.java
+++ src/main/java/Classes/ItemIdentification.java
@@ -1,26 +1,27 @@
 package Classes;
+class ItemIdentification
+{
+    private String itemName;
+    private String itemId;
 
-import java.util.ArrayList;
-
-public class ItemIdentification {
-    private Integer barcode;
-
-    public ItemIdentification(Integer barcode) {
-        this.barcode = barcode;
-        //this.item = lookupItem(barcode);
+    public ItemIdentification(String itemName, String itemId) {
+        this.itemName = itemName;
+        this.itemId = itemId;
     }
 
-    static public Item lookupItem(Integer barcode) throws ItemNotFoundException {
-        //Looks through all items in the itemList and returns the item with the same barcode
-        for (Item item : Item.getItemList()) {
-            if (item.getBarcode().equals(barcode)) {
-                String r = item.toString() + item.getName() + item.getId();
-                System.out.print(r);
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public Item identification(Integer barcode) throws ItemNotFoundException {
+        for(Item item : Item.getItemList()) {
+            if(item.getBarcode() == barcode) {
                 return item;
             }
-        }
-        // If item is not found, throw an exception
-        throw new ItemNotFoundException("Item not found for barcode: " + barcode);
+        } throw new ItemNotFoundException("Item not found Barcode: " + barcode);
     }
-
 }
